a972a66e56e6d9c772f62f9606d4d35d4ae33d39
dkkret <dk.kret@gmail.com>
2022-05-24 07:49:00 +0200

    done project

--- index.js
+++ index.js
@@ -21,8 +21,17 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  //res.json({greeting: 'hello API'});
+  const ip = req.ip
+  const lang = req.headers['accept-language']
+  const soft = req.headers['user-agent']
+
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: soft
+  })
 });
 
 
